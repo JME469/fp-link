f0c78d6aec6170ccc7d6fecbf87e0d20e0cf3f98
JME <jaimeaguera469@gmail.com>
2024-04-22 10:47:40 +0200

    Test commit

---
+++ src/components/headerComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="header-container">
+    <div>
+      <a href="" id="logo">FPLink</a>
+    </div>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@/styles/vars.css';
+#header-container{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 30px;
+
+  min-height: 450px;
+
+  background-color: var(--lightGreen);
+}
+
+nav {
+  padding: 10px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  display: inline;
+  margin-right: 10px;
+}
+
+a {
+  color: var(--black);
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+#logo{
+  font-family: Syne;
+  font-weight: 800;
+  letter-spacing: 2px;
+}
+</style>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
 
+<style scoped>
+</style>
+
 <script>
+
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/headerComponent.vue";
 
 export default {
   name: "HomeView",
