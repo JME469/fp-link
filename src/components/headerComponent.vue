e64b2f824eebe9c2d90f5d774d5300927c45f331
JME <jaimeaguera469@gmail.com>
2024-04-27 13:58:41 +0200

    Header component updated

--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -1,72 +1,129 @@
 <template>
-  <div id="header-container">
-    <div id="logo-container">
-      <a href="" id="logo">FPLink</a>
+  <header class="header">
+    <div class="container">
+      <router-link to="/" class="logo">
+        <img src="@/assets/logo.png" alt="Logo">
+      </router-link>
+      <nav :class="{ 'nav-open': isNavOpen }">
+        <ul>
+          <li><router-link to="/">Inicio</router-link></li>
+          <li><router-link to="/estudiantes">Estudiantes</router-link></li>
+          <li><router-link to="/empresas">Empresas</router-link></li>
+          <li><router-link to="/about">Acerca de</router-link></li>
+          <li><router-link to="/contact">Contacto</router-link></li>
+        </ul>
+        <button class="menu-toggle" @click="toggleNav">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </nav>
     </div>
-    <nav>
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-      </ul>
-    </nav>
-  </div>
+  </header>
 </template>
 
-<style scoped lang="scss">
-@import '@/styles/vars.css';
-#header-container{
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  gap: 30px;
+<style lang="scss" scoped>
+.header {
+  background-color: #333;
+  color: #fff;
+  padding: 10px 0;
 
-  min-height: 450px;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-  background-color: var(--lightGreen);
-}
+    .logo img {
+      width: 100px; /* Adjust the width as needed */
+    }
+  }
 
-#logo-container{
-  margin-top: 20px;
-}
+  nav {
+    display: flex;
+    align-items: center;
 
-nav {
-  padding: 10px;
-  margin-top: 10px;
-}
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
 
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-}
+      li {
+        margin-right: 20px;
 
-li {
-  display: inline;
-  margin-right: 10px;
-}
+        &:last-child {
+          margin-right: 0;
+        }
 
-a {
-  color: var(--black);
-  text-decoration: none;
-  font-family: Montserrat;
-  font-weight: 600;
-}
+        a {
+          color: #fff;
+          text-decoration: none;
 
-a:hover {
-  text-decoration: underline;
-}
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
 
-#logo{
-  font-family: Syne;
-  font-weight: 800;
-  letter-spacing: 2px;
+    .menu-toggle {
+      display: none; /* Hide by default */
+
+      span {
+        display: block;
+        width: 25px;
+        height: 3px;
+        background-color: #fff;
+        margin-bottom: 5px;
+      }
+    }
+  }
+
+  /* Media query for mobile */
+  @media (max-width: 768px) {
+    nav {
+      display: none; /* Hide nav by default */
+      flex-direction: column;
+      align-items: flex-start;
+      width: 100%;
+      padding-top: 10px;
+      background-color: #333;
+
+      &.nav-open {
+        display: flex; /* Show nav when isNavOpen is true */
+      }
+
+      .menu-toggle {
+        display: block; /* Show menu toggle button */
+        cursor: pointer;
+        padding: 10px;
+        background-color: transparent;
+        border: none;
+
+        span {
+          display: block;
+          width: 25px;
+          height: 3px;
+          background-color: #fff;
+          margin-bottom: 5px;
+        }
+      }
+    }
+  }
 }
 </style>
 
 <script>
 export default {
-  name: "Header",
-};
+  name: 'Header',
+  data() {
+    return {
+      isNavOpen: false
+    };
+  },
+  methods: {
+    toggleNav() {
+      this.isNavOpen = !this.isNavOpen;
+    }
+  }
+}
 </script>
